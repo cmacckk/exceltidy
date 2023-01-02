7b6201276ea8f0b64b5ddf0ad35268a1f990b763
cmacckk <emailforgty@163.com>
2023-01-02 21:03:42 +0800

    remove classifiers OS

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3) ",
-        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',    #对python的最低版本要求
 )
